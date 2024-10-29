f024f00408a9f81152e33a5773b638decfdda7f0
cowwow <gooodliu@qq.com>
2024-10-29 15:19:24 +0800

    Site updated: 2024-10-29 15:19:22

--- js/utils.js
+++ js/utils.js
@@ -37,7 +37,6 @@
             }
         },
         snackbarShow: (text, showAction = false, duration = 5000) => {
-            document.styleSheets[0].addRule(':root', `--efu-snackbar-time:${duration}ms!important`);
             Snackbar.show({
                 text,
                 showAction,
@@ -101,12 +100,6 @@
         },
         isMobile: () => /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent),
         isHidden: e => 0 === e.offsetHeight && 0 === e.offsetWidth,
-        addEventListenerPjax: (ele, event, fn, option = false) => {
-            ele.addEventListener(event, fn, option)
-            utils.addGlobalFn('pjax', () => {
-                ele.removeEventListener(event, fn, option)
-            })
-        },
         animateIn: (ele, text) => {
             Object.assign(ele.style, {display: 'block', animation: text});
         },
